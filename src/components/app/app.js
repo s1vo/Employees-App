1b2419ff41401f54b7aae0fbb806daecc0921c02
Michael Sivokon <sivokonma@gmail.com>
2022-04-19 14:56:31 +0300

    add new function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App(){
 
     const data = [
-        {name: 'Misha S.', salary: 25000, increase: true},
-        {name: 'Alexcande B.', salary: 8000, increase: false },
-        {name: 'Pavel V.', salary: 3000, increase: false }
+        {name: 'Misha S.', salary: 25000, increase: true, id:1 },
+        {name: 'Alexcande B.', salary: 8000, increase: false, id:2  },
+        {name: 'Pavel V.', salary: 3000, increase: false, id:3  }
     ];
 
     return(
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,8 +5,9 @@ import './employees-list.css';
 const EmployeesList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
         return (
-            <EmployeesListItem {...item}/>
+            <EmployeesListItem key={id} {...item}/>
         );
     });
 
